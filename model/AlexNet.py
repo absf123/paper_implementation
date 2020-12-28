83437dcbdd41eb057bed6ceb6a70c3d757c3d78d
absf123 <53507170+absf123@users.noreply.github.com>
2020-12-29 02:02:10 +0900

    AlexNet 구현 끝

--- model/AlexNet.py
+++ model/AlexNet.py
@@ -1,11 +1,49 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as f
+import torch.nn.functional as F
 import numpy
 
 # AlexNet
 class AlexNet(nn.Module):
-    def __init__(self):
-        super(AlexNet, self.__init__())
+    def __init__(self, num_classes=1000):
+        super(AlexNet, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=96, kernel_size=11, stride=4)
+        self.maxpool1 = nn.MaxPool2d(3, 2)
+        self.bn1 = nn.BatchNorm2d(96)
+        self.conv2 = nn.Conv2d(in_channels=96, out_channels=256, kernel_size=5, stride=1, padding=2)
+        self.maxpool2 = nn.MaxPool2d(3, 2)
+        self.bn2 = nn.BatchNorm2d(256)
+        self.conv3 = nn.Conv2d(in_channels=256, out_channels=384, kernel_size=3, stride=1, padding=1)
+        self.conv4 = nn.Conv2d(in_channels=384, out_channels=384, kernel_size=3, stride=1, padding=1)
+        self.conv5 = nn.Conv2d(in_channels=384, out_channels=256, kernel_size=3, stride=1, padding=1)
+        self.maxpool3 = nn.MaxPool2d(3, 2)
+        self.lin1 = nn.Linear(6*6*256, 4096)
+        self.lin2 = nn.Linear(4096, 4096)
+        self.lin3 = nn.Linear(4096, 1000)
 
+    def forward(self, x):
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = self.maxpool1(x)
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = self.maxpool2(x)
+        x = F.relu(self.conv3(x))
+        x = F.relu(self.conv4(x))
+        x = F.relu(self.conv5(x))
+        x = self.maxpool3(x)
+        print(x.shape)
+        x = x.flatten(1)
+        print(x.shape) # torch.Size([2, 43264])
+        x = F.relu(F.dropout2d(self.lin1(x), p=0.5))
+        x = F.relu(F.dropout2d(self.lin2(x), p=0.5))
+        out = F.relu(self.lin3(x))
+
+        return out
+
+
+if __name__ == "__main__":
+    from torchsummary import summary
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    net = AlexNet().to(device)
+    summary(net, (3, 227, 227), 30)
 
